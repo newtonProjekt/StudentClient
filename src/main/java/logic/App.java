5eced9887e3a2b238ff60b540476cfa6b1e2e103
Johan Lindström <jolindse@hotmail.com>
2016-03-16 14:44:32 +0100

    Network init changed.

--- src/main/java/logic/App.java
+++ src/main/java/logic/App.java
@@ -15,102 +15,106 @@ import java.util.List;
 
 /**
  * Application entry class. Starts network and login routines.
- *
+ * <p>
  * Created by Johan Lindström (jolindse@hotmail.com) on 2016-03-11.
  */
-public class App extends Application{
-	private SelectExam selectEx;
-	private SchoolTest currTest;
-	private CommandHandler ch;
-	private LoginBox loginBox;
-	private Stage primaryStage;
-	private String loginName, password;
+public class App extends Application {
+    private SelectExam selectEx;
+    private SchoolTest currTest;
+    private CommandHandler ch;
+    private LoginBox loginBox;
+    private Stage primaryStage;
+    private String loginName, password;
 
-	private boolean loggedIn = false;
+    private boolean loggedIn = false;
 
-	public static void main(String[] args) {
-		launch(args);
-	}
-	/**
-	 * Inits the network loop and commandhandler.
-	 *
-	 * @throws Exception
-	 */
-	@Override
-	public void init() throws Exception {
-		super.init();
-		NetworkConnection nc = new NetworkConnection("localhost",3004);
-		ch = new CommandHandler(this);
-		nc.setCommandHandler(ch);
-		Thread networkThread = new Thread(nc);
-		networkThread.start();
-		
-	}
+    public static void main(String[] args) {
+        launch(args);
+    }
 
-	/**
-	 * Shows loginbox and waits for login.
-	 *
-	 * @param primaryStage
-	 * @throws Exception
-	 */
-	@Override
-	public void start(Stage primaryStage) throws Exception {
+    /**
+     * Inits the network loop and commandhandler.
+     *
+     * @throws Exception
+     */
+    @Override
+    public void init() throws Exception {
+        super.init();
+        initNetwork();
+    }
 
-		this.primaryStage = primaryStage;
-		loginBox = new LoginBox(this);
-		loginBox.showAndWait();
+    /**
+     * Shows loginbox and waits for login.
+     *
+     * @param primaryStage
+     * @throws Exception
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception {
 
-	}
-	/**
-	 * Should send the list of available tests to the selectexam routine.
-	 *
-	 * @param availableTests
-	 */
-	public void showTestBox(List<SchoolTest> availableTests){
-		Platform.runLater(() ->{
-			SelectExam selectGui = new SelectExam(primaryStage, availableTests);
-		});
-	}
+        this.primaryStage = primaryStage;
+        loginBox = new LoginBox(this);
+        loginBox.showAndWait();
 
-	// LOGIN METHODS
+    }
 
-	/**
-	 * Send logincommand if user is not allready logged in.
-	 *
-	 * @param persNumber
-	 * @param password
-	 */
-	public void doLogin(String persNumber, String password){
-		loginName = persNumber;
-		this.password = password;
-		if (!loggedIn) {
-			Login currLogin = new Login(loginName, this.password, true);
-			ch.send("login", currLogin);
-		}
-	}
+    /**
+     * Should send the list of available tests to the selectexam routine.
+     *
+     * @param availableTests
+     */
+    public void showTestBox(List<SchoolTest> availableTests) {
+        Platform.runLater(() -> {
+            SelectExam selectGui = new SelectExam(primaryStage, availableTests);
+        });
+    }
 
-	/**
-	 * Sets the login ok.
-	 */
-	public void loginOk(){
-		loggedIn=true;
-		Platform.runLater(() ->{
-			loginBox.close();
-			primaryStage.show();
-		});
+    // LOGIN METHODS
 
-	}
+    /**
+     * Send logincommand if user is not allready logged in.
+     *
+     * @param persNumber
+     * @param password
+     */
+    public void doLogin(String persNumber, String password) {
+        loginName = persNumber;
+        this.password = password;
+        if (!loggedIn) {
+            Login currLogin = new Login(loginName, this.password, true);
+            ch.send("login", currLogin);
+        }
+    }
 
-	/**
-	 * Shows error message if login failed.
-	 */
-	public void loginFailed(){
-		loginBox.setErrorLabel("Login misslyckades.");
-	}
+    /**
+     * Sets the login ok.
+     */
+    public void loginOk() {
+        loggedIn = true;
+        Platform.runLater(() -> {
+            loginBox.close();
+            primaryStage.show();
+        });
 
-	public void doTest(SchoolTest test){
-		currTest = test;
+    }
 
-	}
+    /**
+     * Shows error message if login failed.
+     */
+    public void loginFailed() {
+        loginBox.setErrorLabel("Login misslyckades.");
+    }
 
+    public void doTest(SchoolTest test) {
+        currTest = test;
+
+    }
+
+    private void initNetwork() {
+        NetworkConnection nc = new NetworkConnection("localhost", 3004);
+        ch = new CommandHandler(this);
+        nc.setCommandHandler(ch);
+        Thread networkThread = new Thread(nc);
+        networkThread.start();
+    }
 }
